530cfae1722348d445d62eb907db9733400c95f5
taniasilvana <taniasilvana.lapalelo@wingscorp.com>
2023-06-23 15:40:18 +0700

    current weather screen

--- App.js
+++ App.js
@@ -1,20 +1,45 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { SafeAreaView, StyleSheet, Text, View } from 'react-native';
 
-export default function App() {
+const App = () => {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    <SafeAreaView style={styles.wrapper}>
+      <View style={styles.container}>
+        <Text style={styles.temp}>
+          6
+        </Text>
+        <Text style={styles.feels}>
+          Feels like 5
+        </Text>
+        <Text style={styles.highLow}>
+          High: 8 Low: 6
+        </Text>
+      </View>
+    </SafeAreaView>
+  )
 }
 
 const styles = StyleSheet.create({
+  wrapper: {
+    flex: 1
+  },
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: 'pink',
+    alignItems: 'center'
   },
+  temp: {
+    color: 'black',
+    fontSize: 48
+  },
+  feels: {
+    color: 'black',
+    fontSize: 30
+  },
+  highLow: {
+    color: 'black',
+    fontSize: 20
+  }
 });
+
+export default App
