2130d64c7c5c2294a335f17ca0cc936a77e1fc8c
niatania102 <niatania102@gmail.com>
2023-06-26 15:27:44 +0700

    add component

--- App.js
+++ App.js
@@ -1,45 +1,19 @@
-import React from 'react';
-import { SafeAreaView, StyleSheet, Text, View } from 'react-native';
+import React from "react"
+import { View, StyleSheet } from "react-native"
+import CurrentWeather from "./src/components/currentweather"
 
 const App = () => {
   return (
-    <SafeAreaView style={styles.wrapper}>
-      <View style={styles.container}>
-        <Text style={styles.temp}>
-          6
-        </Text>
-        <Text style={styles.feels}>
-          Feels like 5
-        </Text>
-        <Text style={styles.highLow}>
-          High: 8 Low: 6
-        </Text>
-      </View>
-    </SafeAreaView>
+    <View style={styles.container}>
+      <CurrentWeather/>
+    </View>
   )
 }
 
 const styles = StyleSheet.create({
-  wrapper: {
-    flex: 1
-  },
   container: {
-    flex: 1,
-    backgroundColor: 'pink',
-    alignItems: 'center'
-  },
-  temp: {
-    color: 'black',
-    fontSize: 48
-  },
-  feels: {
-    color: 'black',
-    fontSize: 30
-  },
-  highLow: {
-    color: 'black',
-    fontSize: 20
+    flex: 1
   }
-});
+})
 
 export default App
