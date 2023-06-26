4240eaa49691afbe8a6a107125bf829fd960e0c9
niatania102 <niatania102@gmail.com>
2023-06-26 16:28:39 +0700

    add UpcomingWeather component

---
+++ src/components/UpcomingWeather.js
@@ -0,0 +1,18 @@
+import React from "react"
+import { SafeAreaView, StyleSheet } from "react-native"
+
+const UpcomingWeather = () => {
+    return (
+        <SafeAreaView style={styles.container}>
+            <Text>Upcoming Weather</Text>
+        </SafeAreaView>
+    )
+}
+
+StyleSheet.create({
+    container:{
+
+    }
+})
+
+export default UpcomingWeather
