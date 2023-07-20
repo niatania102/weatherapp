a05896e3e9c97d7b91d85d23062aeb4756e40593
niatania102 <niatania102@gmail.com>
2023-07-20 21:16:27 +0700

    implementing city screen

--- App.js
+++ App.js
@@ -1,6 +1,5 @@
 import React from "react"
 import { View, StyleSheet } from "react-native"
-import UpcomingWeather from "./src/screens/UpcomingWeather"
 import City from "./src/components/City"
 
 const App = () => {
--- src/components/City.js
+++ src/components/City.js
@@ -1,17 +1,73 @@
-import React from "react";
-import { SafeAreaView, Text, StyleSheet } from "react-native/types";
+import React from 'react';
+import { SafeAreaView, Text, ImageBackground, StatusBar, StyleSheet, View } from 'react-native';
+import { Feather } from '@expo/vector-icons'
 
-const city = () => {
+const City = () => {
     return (
         <SafeAreaView style={styles.container}>
-            <Text>City</Text>
+            <ImageBackground
+                source={require('../../assets/city-background.jpg')}
+                style={styles.imageLayout}
+            >
+                <Text style={[styles.cityName, styles.cityText]}>London</Text>
+                <Text style={[styles.countryName, styles.cityText]}>UK</Text>
+                <View style={styles.populationWrapper}>
+                    <Feather name={'user'} size={50} color={'red'} />
+                    <Text style={styles.populationText}>8000</Text>
+                </View>
+                <View style={styles.riseSetWrapper}>
+                    <Feather name={'sunrise'} size={50} color={'white'} />
+                    <Text style={styles.riseSetText}>10:12:34am</Text>
+                    <Feather name={'sunset'} size={50} color={'white'} />
+                    <Text style={styles.riseSetText}>17:34:12pm</Text>
+                </View>
+            </ImageBackground>
         </SafeAreaView>
     )
 }
 
 const styles = StyleSheet.create({
     container:{
+        flex: 1,
+        marginTop: StatusBar.currentHeight || 0
+    },
+    imageLayout:{
         flex: 1
+    },
+    cityName:{
+        fontSize: 40
+    },
+    countryName:{
+        fontSize: 30
+    },
+    cityText:{
+        justifyContent: 'center',
+        alignSelf: 'center',
+        fontWeight: 'bold',
+        color: 'white'
+    },
+    populationWrapper:{
+        flexDirection: 'row',
+        alignItems: 'center',
+        justifyContent: 'center',
+        marginTop: 30
+    },
+    populationText:{
+        fontSize: 25,
+        marginLeft: 7.5,
+        color: 'red',
+        fontWeight: 'bold'
+    },
+    riseSetWrapper:{
+        flexDirection: 'row',
+        alignItems: 'center',
+        justifyContent: 'space-around',
+        marginTop: 30
+    },
+    riseSetText:{
+        fontSize:20,
+        color: 'white',
+        fontWeight: 'bold'
     }
 })
 export default City
