e0703e0c516e9e0512cc856525b41e760de19c23
taniasilvana <taniasilvana.lapalelo@wingscorp.com>
2023-07-21 13:09:21 +0700

    implementing tabs

--- src/screens/UpcomingWeather.js
+++ src/screens/UpcomingWeather.js
@@ -1,6 +1,13 @@
 import React from 'react'
-import { SafeAreaView, StyleSheet, Text, FlatList, StatusBar, ImageBackground } from 'react-native'
-import ListItem from '../components/listitem'
+import { 
+    SafeAreaView, 
+    StyleSheet, 
+    Text, 
+    FlatList, 
+    StatusBar, 
+    ImageBackground 
+} from 'react-native'
+import ListItem from '../components/ListItem'
 
 const DATA = [
     {
