dd6e1884cbe502983ce2af0be716700568f9d64f
niatania102 <niatania102@gmail.com>
2023-07-22 17:07:08 +0700

    add loading state

--- App.js
+++ App.js
@@ -1,4 +1,5 @@
-import React from "react"
+import React, { useState } from "react"
+import { ActivityIndicator, View, StyleSheet } from "react-native"
 import { NavigationContainer } from "@react-navigation/native"
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs"
 import Tabs from "./src/components/Tabs"
@@ -7,11 +8,28 @@ import Counter from "./src/demontrations/Counter"
 const Tab = createBottomTabNavigator()
 
 const App = () => {
+  const [loading, setLoading] = useState(true)
+  if(loading){
+    console.log("masuk sini")
+    return (
+      <View style={styles.container}>
+        <ActivityIndicator
+          size={'large'}
+          color={'blue'}
+        />
+      </View>
+    )
+  }
   return (
     <NavigationContainer>
-      <Counter/>
+      <Tabs/>
     </NavigationContainer>
   )
 }
-
+const styles = StyleSheet.create({
+  container:{
+    justifyContent: 'center',
+    flex: 1
+  }
+})
 export default App
