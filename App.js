509ac2a725c5cf2db99688264eadf822ee03e761
niatania102 <niatania102@gmail.com>
2023-07-22 17:37:56 +0700

    get user location

--- App.js
+++ App.js
@@ -1,14 +1,34 @@
-import React, { useState } from "react"
+import React, { useState, useEffect } from "react"
 import { ActivityIndicator, View, StyleSheet } from "react-native"
 import { NavigationContainer } from "@react-navigation/native"
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs"
 import Tabs from "./src/components/Tabs"
 import Counter from "./src/demontrations/Counter"
+import * as Location from 'expo-location';
 
 const Tab = createBottomTabNavigator()
 
 const App = () => {
   const [loading, setLoading] = useState(true)
+  const [location, setLocation] = useState(null)
+  const [error, setError] = useState(null)
+
+  useEffect(() => {
+    (async() => {
+      let { status } = await Location.requestForegroundPermissionsAsync()
+      if (status !== 'granted'){
+        setError('permission to access location was denied')
+        return
+      }
+      let location = await Location.getCurrentPositionAsync({})
+      setLocation(location)
+    })()
+  }, [])
+
+  if(location){
+    console.log(location)
+  }
+
   if(loading){
     console.log("masuk sini")
     return (
