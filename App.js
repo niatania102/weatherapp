6c1e4d3c04ce47aa40a4e9c746b65e4a38e569c6
niatania102 <niatania102@gmail.com>
2023-07-23 04:15:49 +0700

    make own hooks

--- App.js
+++ App.js
@@ -1,57 +1,14 @@
 import React, { useState, useEffect } from "react"
 import { ActivityIndicator, View, StyleSheet } from "react-native"
 import { NavigationContainer } from "@react-navigation/native"
-import { createBottomTabNavigator } from "@react-navigation/bottom-tabs"
 import Tabs from "./src/components/Tabs"
-import * as Location from 'expo-location'
-import {WEATHER_API_KEY} from '@env'
-import { weatherType } from "./src/utilities/weatherType"
+import { useGetWeather } from "./src/hooks/useGetWeather"
 
 //api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={API key}
 
 const App = () => {
-  const [loading, setLoading] = useState(true)
-  const [location, setLocation] = useState(null)
-  const [error, setError] = useState(null)
-  const [weather, setWeather] = useState([])
-  const [lat, setLat] = useState([])
-  const [lon, setLon] = useState([])
-
-  const fetchWeatherData = async () => {
-    try{
-      const res = await fetch(
-        `http://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${WEATHER_API_KEY}`
-      )
-      const data = await res.json()
-      setWeather(data)
-      setLoading(false)
-    }
-    catch (error) {
-      setError('could not fetch weather')
-    }
-    finally{
-      setLoading(false)
-    }
-  }
-
-  useEffect(() => {
-    (async() => {
-      let { status } = await Location.requestForegroundPermissionsAsync()
-      if (status !== 'granted'){
-        setError('permission to access location was denied')
-        return
-      }
-      let location = await Location.getCurrentPositionAsync({})
-      setLat(location.coords.latitude)
-      setLon(location.coords.longitude)
-      await fetchWeatherData()
-    })()
-  }, [lat, lon])
-
-  if(weather){
-    console.log(weather)
-  }
-
+  const [loading, error, weather] = useGetWeather()
+  console.log(loading, error, weather)
   if(loading){
     return (
       <View style={styles.container}>
